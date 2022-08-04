79f47c548485f1fce928deede496d2ca12fcdd7a
zerom-h <cupdisin@gmail.com>
2022-08-04 16:17:43 +0900

    암호화 추가

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -1,13 +1,22 @@
 package com.cos.security1.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 됨
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다.
+    @Bean
+    public BCryptPasswordEncoder encoderPwd() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
@@ -18,6 +27,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().permitAll()                                                                                   //그 외 다른 주소는 허용
                 .and()
                 .formLogin()                                                                                                //formLogin 방식 사용
-                .loginPage("/login");                                                                                       //antMatchers 적용된 경로 이동시 loginPage로 이동
+                .loginPage("/loginForm");                                                                                   //antMatchers 적용된 경로 이동시 loginPage로 이동
     }
 }
--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -1,11 +1,22 @@
 package com.cos.security1.controller;
 
+import com.cos.security1.model.User;
+import com.cos.security1.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
+
+    private final UserRepository userRepository;
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @GetMapping({"", "/"})
     public String index() {
         return "index";
@@ -18,14 +29,26 @@ public class IndexController {
     }
 
     //SecurityConfig 설정전에는 주소를 낚아챔, 생성 후에는 작동 안함
-    @GetMapping("/login")
+    @GetMapping("/loginForm")
     public String login() {
         return "loginForm";
     }
 
-    @GetMapping("/join")
-    @ResponseBody
-    public String join() {
-        return "join";
+    @GetMapping("/joinForm")
+    public String joinForm() {
+        return "joinForm";
     }
+
+    @PostMapping("/join")
+    public String join(User user) {
+        System.out.println(user);
+        user.setRole("ROLE_USER");
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        userRepository.save(user); // 회원가입은 됨 비밀번호 : 1234 => 시큐리티로 로그인 할 수 없음. 이유는 패스워드가 암호화가 안되서
+        return "redirect:/loginForm";
+    }
+
+
 }
